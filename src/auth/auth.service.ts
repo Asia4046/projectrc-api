b03e7822ae250c5caa8849b7a5f509bab546804b
Asia4046 <kirito234875@gmail.com>
2024-12-17 19:00:38 +0530

    lint files

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,29 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { AuthDto } from "./dto";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 import * as argon from 'argon2';
 
 @Injectable({})
-export class AuthService{
-    constructor(private prisma: PrismaService){}
-    async signup(dto: AuthDto) {
-        // Password Hash
-        const hash = await argon.hash(dto.password);
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    // Password Hash
+    const hash = await argon.hash(dto.password);
 
-        // Save USER
-        const user = await this.prisma.user.create({
-            data:{
-                email: dto.email,
-                hash,
-            }
-        });
+    // Save USER
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
 
-        delete user.hash;
+    delete user.hash;
 
-        return user;
-    }
+    return user;
+  }
 
-    signin() {
-        return { msg: "Welcome to project-rc's auth page {signin} !" }
-    }
+  signin() {
+    return { msg: "Welcome to project-rc's auth page {signin} !" };
+  }
 }
