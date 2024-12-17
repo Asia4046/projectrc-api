57e051c315d8795eaffa8dc147c44f1e8c342925
Asia4046 <kirito234875@gmail.com>
2024-12-17 18:59:12 +0530

    nestjs global pipe

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService{
     constructor(private prisma: PrismaService){}
-    signup() {
-        return { msg: "Welcome to project-rc's auth page {signup} !" }
+    async signup(dto: AuthDto) {
+        // Password Hash
+        const hash = await argon.hash(dto.password);
+
+        // Save USER
+        const user = await this.prisma.user.create({
+            data:{
+                email: dto.email,
+                hash,
+            }
+        });
+
+        delete user.hash;
+
+        return user;
     }
 
     signin() {
