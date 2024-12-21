23a73484c200a203af9f263f3f15bf782456b946
Asia4046 <kirito234875@gmail.com>
2024-12-21 19:10:14 +0530

    Update READNE

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+    @Get('me')
+    getMe() {
+        return({
+            server: 'NestJS Test Server Running on AWS',
+            version: 'os_prod_linux_1.0.001-alpha',
+            status: 200
+        })
+    }
+}
